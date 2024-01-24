61849ca25b7735e98f7bd2508ae130da4a3bad5a
Alexandre Lecoeur <alexandre.lecoeur.dev@gmail.com>
2024-01-24 11:15:34 +0100

    init commit

---
+++ src/main/java/al/test/exercice_alten/ExerciceAltenApplication.java
@@ -0,0 +1,13 @@
+package al.test.exercice_alten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceAltenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciceAltenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/test/exercice_alten/ExerciceAltenApplicationTests.java
@@ -0,0 +1,13 @@
+package al.test.exercice_alten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceAltenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
