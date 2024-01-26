af220227fe0f663126f0f7b9932fb39da25b0e00
Alexandre Lecoeur <alexandre.lecoeur.dev@gmail.com>
2024-01-26 12:04:51 +0100

    add all exception in exceptionHandler

--- src/main/java/al/test/exercice_alten/exception/CustomExceptionHandler.java
+++ src/main/java/al/test/exercice_alten/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package al.test.exercice_alten.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -14,6 +15,12 @@ import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        String errorMessage = ex.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
@@ -25,5 +32,11 @@ public class CustomExceptionHandler {
         }
 
         return ResponseEntity.badRequest().body(errors);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        String errorMessage = "The code must be unique.";
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
